31ad8fdc453d5bf312cb504cc7a0480b984fa47b
Jesús del Ángel <delangelj711@gmail.com>
2024-03-05 20:07:09 -0600

    delete unused files

--- com/example/demosoap/gen/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.com/demosoap/gen", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.example.demosoap.gen;
